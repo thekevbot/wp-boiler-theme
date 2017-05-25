ce48ea685eb9bebd6ee2f1565ce08e2a60cc5f5c
Kevin Haag <khaag06@gmail.com>
2017-05-25 10:00:47 -0700

    Add empty state to basic index

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php get_header();?>
 
-    <!-- The most basic of loops. Go forth and EXPAND IT. -->
-    <?php if (have_posts()) : while (have_posts()) : the_post();?>
-        <h1><?php the_title(); ?></h1>
-        <?php the_content(); ?>
-    <?php endwhile; endif; ?>
+<!-- The most basic of loops. Go forth and EXPAND IT. -->
+<?php if (have_posts()) : while (have_posts()) : the_post();?>
+    <h1><?php the_title(); ?></h1>
+    <?php the_content(); ?>
+<?php endwhile; else : ?>
+    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
 
 <?php get_footer();?>
